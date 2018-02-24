b342b5dba6acf86c2efbab2b0de4739943a7c284
gijoncheng <luojio@oregonstate.edu>
2018-02-23 23:59:06 -0800

    updated url css

--- myTheme/page.php
+++ myTheme/page.php
@@ -3,42 +3,33 @@
 		<div class="content">
 
 			<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
+				<div id="post-<?php the_ID(); ?>" <?php post_class( 'single single-post' ); ?>>
 					
-					<a class="back_btn" href="/mySite/">
+					<a class="back_btn" href="<?php bloginfo('url'); ?>">
 						back
 					</a>
 
-					<div class="content">
-						
-						<?php 
-							$post_format = get_post_format();
-						?>
-						
-						<div class="post-header">
-							<h2>
-								<?php the_title(); ?>
-							</h2>
-							<p class="grey-text lighten-1">
-								Last update: <?php the_time( get_option( 'date_format' ) ); ?>
-							</p>
-						</div>
-
-						<div class="divider"></div>
-
-						<div class="post-inner">
-							<div class="post-content">
-							
-								<?php the_content(); ?>
-							
-							</div><!-- .post-content -->
-							
-						
-						</div><!-- .post-inner -->
-						
-						<div class="divider"></div>
-
+					<?php 
+						$post_format = get_post_format();
+					?>
 					
-					</div><!-- .post-container -->
+					<div class="post-header">
+						<h2>
+							<?php the_title(); ?>
+						</h2>
+						<p class="grey-text lighten-1">
+							Last update: <?php the_time( get_option( 'date_format' ) ); ?>
+						</p>
+					</div>
+
+					<div class="divider"></div>
+
+					<div class="post-inner">
+						<div class="post-content">
+							<?php the_content(); ?>
+						</div><!-- .post-content -->
+						
+					</div><!-- .post-inner -->
 					
 				</div><!-- .post -->
 				
--- myTheme/single.php
+++ myTheme/single.php
@@ -2,60 +2,59 @@
 	<div class="container">
 		<div class="content">
 			<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
-			<div id="post-<?php the_ID(); ?>" <?php post_class( 'single single-post' ); ?>>
-				
-				<a class="back_btn" href="/mySite/category/projects">
-					back
-				</a>
-
+				<div id="post-<?php the_ID(); ?>" <?php post_class( 'single single-post' ); ?>>
 					
-					<?php 
-						$post_format = get_post_format();
-					?>
-					
-					<div class="post-header">
-						<h2>
-							<?php the_title(); ?>
-						</h2>
-						<p class="grey-text lighten-1">
-							Last update: <?php the_time( get_option( 'date_format' ) ); ?>
-						</p>
-					</div>
+					<a class="back_btn" href="<?php bloginfo('url');?>/category/projects">
+						back
+					</a>
 
-					<div class="divider"></div>
-
-					<div class="post-inner">
-						<div class="post-content">
-							<?php the_content(); ?>
-						</div><!-- .post-content -->
+						<?php 
+							$post_format = get_post_format();
+						?>
 						
-						<div class="post-meta">
-					
-							<?php if ( has_category() ) : ?>
-								<p class="categories">
-									<?php the_category( ', ' ); ?>
-								</p>
-							<?php endif; ?>
-							
-							<?php if ( has_tag() ) : ?>
-								<p class="tags">
-									<?php the_tags( '', ' ' ); ?>
-								</p>
-							<?php endif; ?>
-							
-							<?php edit_post_link( 'Edit Post', '<p class="post-edit">', '</p>' ); ?>
-		
-						</div><!-- .post-meta -->
-					
-					</div><!-- .post-inner -->
-					
-					<div class="divider"></div>
+						<div class="post-header">
+							<h2>
+								<?php the_title(); ?>
+							</h2>
+							<p class="grey-text lighten-1">
+								Last update: <?php the_time( get_option( 'date_format' ) ); ?>
+							</p>
+						</div>
 
-					<?php comments_template( '', true ); ?>
-				
-				</div><!-- .post-container -->
-				
-			</div><!-- .post -->
+						<div class="divider"></div>
+
+						<div class="post-inner">
+							<div class="post-content">
+								<?php the_content(); ?>
+							</div><!-- .post-content -->
+							
+							<div class="post-meta">
+						
+								<?php if ( has_category() ) : ?>
+									<p class="categories">
+										<?php the_category( ', ' ); ?>
+									</p>
+								<?php endif; ?>
+								
+								<?php if ( has_tag() ) : ?>
+									<p class="tags">
+										<?php the_tags( '', ' ' ); ?>
+									</p>
+								<?php endif; ?>
+								
+								<?php edit_post_link( 'Edit Post', '<p class="post-edit">', '</p>' ); ?>
+			
+							</div><!-- .post-meta -->
+						
+						</div><!-- .post-inner -->
+						
+						<div class="divider"></div>
+
+						<?php comments_template( '', true ); ?>
+					
+					</div><!-- .post-container -->
+					
+				</div><!-- .post -->
 				
 			
 			<?php 
